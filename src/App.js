ec21a31762ae81d0556b914e9f82fdc7ace9e5d3
caffeee17k <andreydossantos17@gmail.com>
2019-05-18 21:26:00 -0300

    Site em reactJS e bootstrap

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { BrowserRouter, Route } from 'react-router-dom'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Rodape from './components/Rodape';
+import Cabecalho from './components/Cabecalho'
+import About from './components/About'
+import Services from './components/Services'
+import Portfolio from './components/Portfolio'
+import Pricing from './components/Pricing'
+import Contact from './components/Contact'
+
+class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <div className="App">
+          <Cabecalho />
+
+          <Route path='/' exact component={About} />
+          <Route path='/services' component={Services} />
+          <Route path='/portfolio' component={Portfolio} />
+          <Route path='/pricing' component={Pricing} />
+          <Route path='/contact' component={Contact} />
+          
+          <Rodape />
+        </div>
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
